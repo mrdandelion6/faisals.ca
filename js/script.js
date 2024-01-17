7c442eec114082e23a97436a79fe6aea18a2c2ca
faisal375 <144740367+faisal375@users.noreply.github.com>
2024-01-17 14:39:17 -0500

    created transparency fade in function for background blending

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const openButton = document.getElementById('openbutton');
+const popMenu = document.getElementById('popmenu');
+const menuButtons = popMenu.querySelectorAll('a');
+const headBar = document.querySelector("#head-area");
+
+let barTransparency = 1;
+
 openButton.addEventListener('click', function () {
     popMenu.classList.toggle('hidden');
 });
@@ -7,3 +14,13 @@ menuButtons.forEach(function(button) {
         popMenu.classList.add('hidden');
     });
 });
+
+// transitionTopBar();
+
+function transitionTopBar() {
+    barTransparency = Math.max(0, barTransparency - 0.05);
+    headBar.style.backgroundColor = `rgba(0, 0, 0, ${barTransparency})`;
+    if (barTransparency > 0) {
+        setTimeout(transitionTopBar, 10);
+    }
+}
