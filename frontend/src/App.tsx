5798b3fa60931c59a375151268a0555b291a30ef
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-07-19 21:10:49 -0400

    content change: added some basic buttons for navbar in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
-import { useState } from 'react'
+import NavBar, { type NavBarButtonProps } from './components/NavBar'
 
 function App() {
+  const leftButtons: NavBarButtonProps[] = [
+    { children: "Home", href: "/" },
+    { children: "About", href: "/about" },
+    { children: "Projects", href: "/projects" }
+  ]
+
+  const rightButtons: NavBarButtonProps[] = [
+    { children: "Contact", href: "/contact" },
+  ]
+
   return (
     <>
-      <p className="body-text">Vite + React</p>
+      <NavBar leftButtons={leftButtons} rightButtons={rightButtons} />
+
+      <main className="pt-16">
+        <p className="body-text">
+          hey , how's it going. <br />
+        </p>
+      </main>
+
     </>
   )
 }
