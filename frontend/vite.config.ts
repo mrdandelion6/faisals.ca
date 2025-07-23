f2ce0b6cf6c343aae7640d70e1039a4ade01bd55
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2025-07-23 00:23:18 -0400

    config change frontend: added base and build keys for vite config , needed for deploying to prod on gh-pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
